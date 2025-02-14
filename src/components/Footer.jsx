0003403f8110ded02c3315cc63504f45a4c11dc6
VivianeCG <chenet.viviane@gmail.com>
2025-02-14 17:17:55 +0100

    suppression dossier inutile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,6 @@ function Footer({src}) {
        <footer>
         <section >
             <p className="footer-content"><span>Made with React</span><img src={reactLogo} alt="logo React"></img></p>
-            <p className="footer-content"><a href="mailto:chenet.viviane@gmail.com"><img src={email} alt="lien pour envoyer un e-mail" /></a><img src={linkedin} alt="logo LinkedIn" /><img src={github} alt="logo GitHub" /></p>
             <p className="footer-content"><Link to='/mentionslegales'>Mentions légales</Link></p>
             <p className="footer-content">© Chenet Viviane - 2025.</p>
         </section>
