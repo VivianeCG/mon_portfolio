6598ae5dff836310029986baa7e76aa226b383e4
VivianeCG <chenet.viviane@gmail.com>
2025-02-14 09:01:19 +0100

    CSS contact

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,12 +1,13 @@
 import copy from "../assets/icons/32px/copy-32.png"
 
+
 function Contactform() {
     return(
         <section id="contact">
             <h2>Contact</h2>
             <div className="contact-elements">
-                <p><a href="mailto:chenet.viviane@gmail.com">Contactez-moi !</a></p>
-                <p>chenet.viviane@gmail.com <button><img src={copy} alt="" /></button></p>
+                <p className="mailto"><a href="mailto:chenet.viviane@gmail.com" className="mailto-link">Envoyer un e-mail </a></p>
+                <p className="copy-mail">chenet.viviane@gmail.com <button className="copy-mail-button"><img src={copy} alt="bouton pour copier l'e-mail" /></button></p>
             </div>
         </section>
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,7 +14,7 @@ function Home() {
             <Skills/>
             <Techs/>
             <Projects/>
-
+            <Contactform/>
         </main>
         </>
             )
