17949c838516d55872717d6267a37b140777c889
VivianeCG <chenet.viviane@gmail.com>
2025-02-17 19:09:57 +0100

    responsive

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,4 +1,5 @@
 import copy from "../assets/icons/32px/copy-32.png"
+import copy16 from "../assets/icons/16px/copy-16.png"
 
 
 function Contactform() {
@@ -7,7 +8,7 @@ function Contactform() {
             <h2>Contact</h2>
             <div className="contact-elements">
                 <p className="mailto"><a href="mailto:chenet.viviane@gmail.com" className="mailto-link">Envoyer un e-mail </a></p>
-                <p className="copy-mail">chenet.viviane@gmail.com <button className="copy-mail-button"><img src={copy} alt="bouton pour copier l'e-mail" /></button></p>
+                <p className="copy-mail">chenet.viviane@gmail.com <button className="copy-mail-button"><img src={copy} alt="bouton pour copier l'e-mail" className="copy-icon" /><img src={copy16} alt="bouton pour copier l'e-mail" className="copy-icon-mobile"/></button></p>
             </div>
         </section>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import burger from "../assets/icons/32px/menu-color-32.png"
+import cross from "../assets/icons/32px/cross-color-32.png"
 
 function Navbar() {
     return(
-        <nav className="navbar">
+        <nav className="navbar ">
             <div className="responsive-menu">
-                <img src={burger} alt="accès au menu" className="burger-icon"/>
+                <img src={burger} alt="accès au menu" className="burger-icon" id="burger"/>
+                <img src={cross} alt="fermer le menu" className="cross-icon-open-menu" id="cross"/>
             <ul>
                 <li className="menu-item"><Link to='/'>Viviane Chenet</Link></li>
                 <li className="menu-item"><a href="#introduction">Présentation</a></li>
