042593d7c96250877288aceff72919d57402a634
VivianeCG <chenet.viviane@gmail.com>
2025-02-19 11:56:35 +0100

    ouverture du menu en responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,35 @@
-import { Link } from "react-router-dom"
-import burger from "../assets/icons/32px/menu-color-32.png"
-import cross from "../assets/icons/32px/cross-color-32.png"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import burger from "../assets/icons/32px/menu-color-32.png";
+import cross from "../assets/icons/32px/cross-color-32.png";
 
 function Navbar() {
-    return(
-        <nav className="navbar ">
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="navbar">
             <div className="responsive-menu">
-                <button className="burger-icon-button"><img src={burger} alt="accès au menu" className="burger-icon" id="burger"/></button>
-                <button className="cross-icon-open-menu-button"><img src={cross} alt="fermer le menu" className="cross-icon-open-menu" id="cross"/></button>
-                <ul>
+                {/* Bouton Burger toujours visible */}
+                <button className="burger-icon-button" onClick={toggleMenu}>
+                    <img 
+                        src={isMenuOpen ? cross : burger} 
+                        alt={isMenuOpen ? "fermer le menu" : "accès au menu"} 
+                        className="burger-icon" 
+                    />
+                </button>
+
+                {/* Menu, caché par défaut en responsive */}
+                <ul className={classNames("navbar", { 
+                    "open-navbar": isMenuOpen,
+                    "closed-navbar": !isMenuOpen,
+                    "visible": isMenuOpen,
+                    "hidden": !isMenuOpen 
+                })}>
                     <li className="menu-item"><Link to='/'>Viviane Chenet</Link></li>
                     <li className="menu-item"><a href="#introduction">Présentation</a></li>
                     <li className="menu-item"><a href="#skills">Compétences</a></li>
@@ -18,7 +39,8 @@ function Navbar() {
                 </ul>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
+
