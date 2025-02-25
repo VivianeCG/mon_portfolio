c505ae821ecd1e1c1ff1861c330ce16de3cecad8
VivianeCG <chenet.viviane@gmail.com>
2025-02-25 15:32:04 +0100

    corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,3 @@
-import reactLogo from "../assets/icons/32px/react-32.png"
-import email from "../assets/icons/32px/email-32.png"
-import linkedin from "../assets/icons/32px/linkedin-color-32.png"
-import github from "../assets/icons/32px/github-32.png"
 import { Link } from "react-router-dom"
 
 function Footer({src}) {
